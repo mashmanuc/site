fb59853ab298d7dec81d9be4d08f986334d61839
Sirko <mashmanuc@gmail.com>
2023-02-26 22:07:15 +0200

    f

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from .models import articls
 
 def base_hom(request):
-    
-    return render(request,'blog/lusa.html')
+    news=articls.objects.all()
+    return render(request,'blog/lusa.html',{'news':news})
